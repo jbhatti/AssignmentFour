8ce82eb825179f6c402986ddfd9ec9025a6ff3f5
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-07-31 12:48:38 -0400

    Initial Commit

---
+++ AssignmentFour/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AssignmentFour
+//
+//  Created by Jaison Bhatti on 2017-07-31.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
